26e187b6a97a5344abcea3ced1eea872d8a0ff97
Kevin Gonzalez <kevin.gonzalez@ses.com>
2024-06-10 02:26:53 +0200

    Disables audio render on server

--- src/helpers/audio.ts
+++ src/helpers/audio.ts
@@ -5,25 +5,31 @@ declare global {
   }
 }
 
-HTMLAudioElement.prototype.playFrom = playFrom;
+const isClient = typeof window !== "undefined";
+
+if (isClient) HTMLAudioElement.prototype.playFrom = playFrom;
 async function playFrom(this: HTMLAudioElement, time = 0) {
   this.currentTime = time;
   this.play();
 }
 
-export const pluckAudio = Audio ? new Audio("/audio/pluck.mp3") : undefined;
+export const pluckAudio = isClient ? new Audio("/audio/pluck.mp3") : undefined;
 
-export const splashAudio = Audio ? new Audio("/audio/splash.mp3") : undefined;
+export const splashAudio = isClient
+  ? new Audio("/audio/splash.mp3")
+  : undefined;
 if (splashAudio) splashAudio.onloadeddata = () => (splashAudio.volume = 0.3);
 
-export const selectAudio = Audio ? new Audio("/audio/select.mp3") : undefined;
+export const selectAudio = isClient
+  ? new Audio("/audio/select.mp3")
+  : undefined;
 if (selectAudio) selectAudio.onloadeddata = () => (selectAudio.volume = 0.2);
 
-export const disabledAudio = Audio
+export const disabledAudio = isClient
   ? new Audio("/audio/disabled.mp3")
   : undefined;
 if (disabledAudio)
   disabledAudio.onloadeddata = () => (disabledAudio.volume = 0.3);
 
-export const levelAudio = Audio ? new Audio("/audio/level.mp3") : undefined;
+export const levelAudio = isClient ? new Audio("/audio/level.mp3") : undefined;
 if (levelAudio) levelAudio.onloadeddata = () => (levelAudio.volume = 0.3);
